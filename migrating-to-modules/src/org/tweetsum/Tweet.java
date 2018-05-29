0460fdb7e233330e7d08e18b1ae03737c3a110d4
Marcos Portela <marcosnasp@gmail.com>
2018-05-29 13:25:48 -0300

    Fix Issue InvalidFormaException

--- migrating-to-modules/src/org/tweetsum/Tweet.java
+++ migrating-to-modules/src/org/tweetsum/Tweet.java
@@ -13,7 +13,8 @@ class Tweet {
     // Sun Apr 03 20:24:49 +0000 2011
     @JsonProperty("created_at")
     @JsonFormat(shape = JsonFormat.Shape.STRING,
-                pattern = "EEE MMM dd HH:mm:ss ZZZZZ yyyy",
+                pattern = "EEE MMM dd HH:mm:ss Z yyyy",
+                locale = "en",
                 timezone = "GMT")
     private Timestamp time;
 
